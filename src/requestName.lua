ec5dbb1c37b3de6930bd7c4f803df14af157fde9
edu1010 <39130123+edu1010@users.noreply.github.com>
2020-10-24 21:15:03 +0200

    letras

--- src/requestName.lua
+++ src/requestName.lua
@@ -7,7 +7,8 @@ local titleH
 local letra1
 local letra2
 local letra3
-local actualChar1
+local actualChar
+local timer
 
 
 function requestName:new(w,h)
@@ -17,85 +18,68 @@ function requestName:new(w,h)
   self.letra1 = "A"
   self.letra2 = "A"
   self.letra3 = "A"
-  self.actualChar1 = 1
-  print("LOAD",self.actualChar1)
+  self.actualChar = 1
+  self.timer = 0.2
 end
 
 
 
 function requestName:update(dt,g)
+  self.timer = self.timer +dt
   --del 65 al 90 es de la A a la Z en codigo assic
-  if self.actualChar1 == 1 then
-    if love.keyboard.isDown("up") then
-      if string.byte(self.letra1)+1 >90 then
-        self.letra1 = string.char(65)
-      else
-        self.letra1 = string.char( string.byte(self.letra1)+1)
-      end
-    elseif love.keyboard.isDown("down") then
-      if  string.byte(self.letra1)-1 < 65 then
-        self.letra1 = string.char(90)
-      else 
-      self.letra1 = string.char( string.byte(self.letra1)-1)
-      end
-    end
+  if self.actualChar == 1 and self.timer > 0.1 then
+    self.timer = 0.0
+    self.letra1 = cambiarLetra(dt,self.letra1)
     if love.keyboard.isDown("left") then
-      self.actualChar1 = 3
-    end
-    if love.keyboard.isDown("right") then
-      self.actualChar1 = 2
-    end
+        self.actualChar = 3
+      end
+      if love.keyboard.isDown("right") then
+        self.actualChar = 2
+      end
   end
   if love.keyboard.isDown("return") then
     g.state = g.gameStates[2]
   end
-  if self.actualChar2 == 2 then
+  if self.actualChar == 2 and self.timer > 0.1 then
   --code
-  if love.keyboard.isDown("up") then
-      --del 65 al 90
-      if string.byte(self.letra2)+1 >90 then
-        self.letra2 = string.char(65)
-      elseif  string.byte(self.letra2)-1 < 65 then
-        self.letra2 = string.char(90)
-      else
-        self.letra2 = string.char( string.byte(self.letra2)+1)
-      end
-      elseif love.keyboard.isDown("down") then
-        self.letra2 = string.char( string.byte(self.letra2)-1)
-      end
-      
-      if love.keyboard.isDown("left") then
-        self.actualChar2 = 1
+  self.timer = 0.0
+  self.letra2 = cambiarLetra(dt,self.letra2)
+    if love.keyboard.isDown("left") then
+        self.actualChar = 1
       end
       if love.keyboard.isDown("right") then
-        self.actualChar2 = 3
+        self.actualChar = 3
       end
   end
-  if self.actualChar3 == 3 then
-  --code
-  if love.keyboard.isDown("up") then
-      --del 65 al 90
-      if string.byte(self.letra3)+1 > 90 then
-        self.letra3 = string.char(65)
-      elseif  string.byte(self.letra3)-1 < 65 then
-        self.letra3 = string.char(90)
-      else
-        self.letra3 = string.char( string.byte(self.letra3)+1)
-      end
-      elseif love.keyboard.isDown("down") then
-        self.letra3 = string.char( string.byte(self.letra3)-1)
-      end
-      
-      if love.keyboard.isDown("left") then
-        self.actualChar3 = 2
+  if self.actualChar == 3 and self.timer > 0.1 then
+    self.timer = 0.0
+    self.letra3 = cambiarLetra(dt,self.letra3)
+    if love.keyboard.isDown("left") then
+        self.actualChar = 2
       end
       if love.keyboard.isDown("right") then
-        self.actualChar3 = 1
+        self.actualChar = 1
       end
+    end
   end
+
+
+function cambiarLetra(dt,letra)
+      if love.keyboard.isDown("up") then
+        if string.byte(letra)+1 >90 then
+          letra = string.char(65)
+        else
+          letra = string.char( string.byte(letra)+1)
+        end
+      elseif love.keyboard.isDown("down") then
+        if  string.byte(letra)-1 < 65 then
+          letra = string.char(90)
+        else 
+        letra = string.char( string.byte(letra)-1)
+        end
+      end
+      return letra
 end
-
-
 function requestName:draw()
   love.graphics.print("PON TU NOMBRE", (self.titleW/2), self.titleH-self.titleH/2,0,1,1)
   love.graphics.print(self.letra1,(self.titleW/2),self.titleH-self.titleH/4,0,1,1)
