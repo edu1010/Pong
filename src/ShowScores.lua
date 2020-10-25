ff6bc8842ff9460d6a9703aaf53a278efb0c86b3
edu1010 <39130123+edu1010@users.noreply.github.com>
2020-10-25 22:39:20 +0100

    score

--- src/ShowScores.lua
+++ src/ShowScores.lua
@@ -26,12 +26,28 @@ function showScores:update(dt,g)
   if(self.actualizar)then
     self.actualizar = false
     local archivo =  io.open("Puntuaciones.txt","r")
-    self.score1 = archivo:read()
-    self.score2 = archivo:read()
-    self.score3 = archivo:read()
-    self.score4 = archivo:read()
-    self.score5 = archivo:read()
-  end
+    local contador = 1
+    for linea in archivo:lines() do
+      if contador == 1 then
+        self.score1 = linea
+        contador = contador+1
+      elseif contador == 2 then
+        self.score2 = linea
+        contador = contador+1
+      elseif contador == 3 then
+        self.score3 = linea
+        contador = contador+1
+      elseif contador == 4 then
+        self.score4 = linea
+        contador = contador+1
+      elseif contador == 5 then
+        self.score5 = linea
+        contador = 1
+      end
+    end
+    archivo.close()
+      
+      end
   if love.keyboard.isDown("return") then
      g.state = g.gameStates[1]
    end
@@ -39,7 +55,11 @@ function showScores:update(dt,g)
 end
 
 function showScores:draw()
-  love.graphics.print(self.score1, (self.scoreW) - (self.scoreH + 60) , (self.scoreH/4),0,2,2)
+  love.graphics.print(self.score1, 0, 0,0,1,1)
+  love.graphics.print(self.score2, 0, 50,0,1,1)
+  love.graphics.print(self.score3, 0, 100,0,1,1)
+  love.graphics.print(self.score4, 0, 150,0,1,1)
+  love.graphics.print(self.score5, 0, 200,0,1,1)
 end
 function showScores:resetScore()
   self.actualizar = true
